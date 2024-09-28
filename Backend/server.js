d20cccb689c397c8e6399806517efa00e64e87f6
M Anas <manas4808294@gmail.com>
2024-09-29 00:30:05 +0500

    server.js updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const dbConnection = require("./db");
 
 const userRoute = require("./routes/userroute");
-const vehcileRoute = require("./routes/vehicleroute")
-const bookingRoute = require("./routes/bookingroute")
-const paymentRoute = require("./routes/paymentroute")
+const vehcileRoute = require("./routes/vehicleroute");
+const bookingRoute = require("./routes/bookingroute");
+const paymentRoute = require("./routes/paymentroute");
 const cookieParser = require("cookie-parser");
 
 var corsOptions = {
-  origin: '*'
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json());
-
 
 // Add cookie parsing middleware
 app.use(cookieParser());
@@ -28,10 +27,17 @@ app.use(cookieParser());
 app.use("/api/user", userRoute);
 app.use("/api/vehicle", vehcileRoute);
 app.use("/api/booking", bookingRoute);
-app.use("/api/payment", paymentRoute)
+app.use("/api/payment", paymentRoute);
 
 app.get("/test", (req, res) => {
   res.send("server working");
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// Catch-all route for serving React's index.html for client-side routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`node server running on port ${port}`));
